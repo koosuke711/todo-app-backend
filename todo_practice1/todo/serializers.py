8d1349678504a263e4417471997a50e038f6f6b7
Kosuke Sugiura <koosuke711@gmail.com>
2024-10-14 03:15:41 +0900

    メールアドレスを追加

--- todo_practice1/todo/serializers.py
+++ todo_practice1/todo/serializers.py
@@ -26,12 +26,13 @@ class ProjectSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password')
+        fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
     
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
+            email=validated_data['email'],
             password=validated_data['password']
         )
         return user
